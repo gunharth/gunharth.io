1357e1d90564a3e351d49c94090f9e07f4db371b
Gunharth Randolf <hello@gunharth.io>
2018-02-06 13:53:03 +0100

    Update npm deps and js requirements

--- public/js/app.js
+++ public/js/app.js
@@ -79,30 +79,6 @@ module.exports = __webpack_require__(15);
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__modules_highlight__ = __webpack_require__(2);
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-// require('./bootstrap');
-
-// window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-// Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-// const app = new Vue({
-//     el: '#app'
-// });
-
-
-
 
 // document.addEventListener('turbolinks:load', () => {
 __WEBPACK_IMPORTED_MODULE_0__modules_highlight__["a" /* default */].start();
