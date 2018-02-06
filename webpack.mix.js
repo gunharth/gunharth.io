55504259fdb43e8c7e065c8b3d6b67131c073c6c
Gunharth Randolf <hello@gunharth.io>
2018-02-06 13:48:16 +0100

    Add tailwindcss minification with purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,18 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
+const glob = require("glob-all");
+const PurgecssPlugin = require("purgecss-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+//
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+    static extract(content) {
+        return content.match(/[A-z0-9-:\/]+/g) || [];
+    }
+}
 
 mix.copyDirectory('content/music-articles/img', 'public/img') // copy music articles assets
    .copyDirectory('content/music-articles/audio', 'public/audio') // copy music articles assets
@@ -36,3 +37,27 @@ mix.webpackConfig({
         new CleanWebpackPlugin(['./public/img', './public/audio'])
     ]
 });
+
+if (mix.inProduction()) {
+    mix.webpackConfig({
+        plugins: [
+            new PurgecssPlugin({
+
+                // Specify the locations of any files you want to scan for class names.
+                paths: glob.sync([
+                    path.join(__dirname, "resources/views/**/*.blade.php"),
+                    path.join(__dirname, "resources/assets/js/**/*.vue")
+                ]),
+                extractors: [
+                    {
+                        extractor: TailwindExtractor,
+
+                        // Specify the file extensions to include when scanning for
+                        // class names.
+                        extensions: ["html", "js", "php", "vue"]
+                    }
+                ]
+            })
+        ]
+    });
+}
