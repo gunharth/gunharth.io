8b6151c4d4fd3734f8673171b63752578381acb4
Gunharth Randolf <hello@gunharth.io>
2018-02-08 21:34:45 +0100

    Fix turbolinks

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,13 +1,16 @@
 import Highlight from './modules/highlight';
+import Turbolinks from 'turbolinks';
+
+Turbolinks.start();
+
+
+
 
 document.addEventListener('turbolinks:load', () => {
+
     Highlight.start();
-});
 
-/* begin begin Back to Top button */
-(function () {
-    'use strict';
-
+    /* begin begin Back to Top button */
     function trackScroll() {
         var scrolled = window.pageYOffset;
         var coords = document.documentElement.clientHeight;
@@ -33,5 +36,6 @@ document.addEventListener('turbolinks:load', () => {
         window.addEventListener('scroll', trackScroll);
         goTopBtn.addEventListener('click', backToTop);
     }
-})();
-/* end begin Back to Top button  */
+    /* end begin Back to Top button  */
+
+});
